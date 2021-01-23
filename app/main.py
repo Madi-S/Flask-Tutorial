2d46bce0aa53c034ff2935afee3ed7ace9f201b2
Madi-S <khovansky99@gmail.com>
2021-01-23 18:49:49 +0600

    posts display

--- app/main.py
+++ app/main.py
@@ -1,7 +1,8 @@
 import view
 from app import app, db
-
+from posts.blueprint import posts
 
 
 if __name__ == '__main__':
+    app.register_blueprint(posts, url_prefix='/blog')
     app.run()
--- app/view.py
+++ app/view.py
@@ -1,11 +1,9 @@
 from app import app
-from models import Post
 from flask import render_template
 
 
 # {'/':'index', '/blog': 'index'}
 @app.route('/')
 def index():
-    my_name = 'Madi'
-    return render_template('index.html', title=Post.query.all())
+    return render_template('index.html')
 
