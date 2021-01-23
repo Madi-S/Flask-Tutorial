84fe33aa47502fe352a009007b358a1e7e4542d5
Madi-S <khovansky99@gmail.com>
2021-01-23 15:03:48 +0600

    links on menubar added

--- app/view.py
+++ app/view.py
@@ -4,7 +4,6 @@ from flask import render_template
 
 # {'/':'index', '/blog': 'index'}
 @app.route('/')
-@app.route('/blog')
 def index():
     my_name = 'Madi'
     return render_template('index.html', name=my_name)
