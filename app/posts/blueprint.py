e2305b3a38bcab6aaa2668091975a00844e5f0e2
Madi-S <khovansky99@gmail.com>
2021-01-24 19:21:44 +0600

    finding posts by tags and display implemented

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask import render_template
-from models import Post
+from models import Post, Tag
 
 
 posts = Blueprint('posts', __name__, template_folder='templates')
@@ -18,5 +18,14 @@ def index():
 @posts.route('/<slug>')
 def post_detail(slug):
     # post not POST request but blog text
-    post = Post.query.filter(Post.slug==slug).first()   
-    return render_template('posts/post_detail.html', post=post)
+    post = Post.query.filter(Post.slug==slug).first()
+    tags = post.tags
+    return render_template('posts/post_detail.html', post=post, tags=tags)
+
+
+# slug for specific 'tags' -> e.g., https://localhost:5000/blog/tag/sports
+@posts.route('/tag/<slug>')
+def tag_detail(slug):
+    tag = Tag.query.filter(Tag.slug==slug).first()
+    posts = tag.posts #.all()
+    return render_template('posts/tag_detail.html',tag=tag, posts=posts)
