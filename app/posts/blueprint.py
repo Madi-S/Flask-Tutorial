e50a0da41cab3077a9f63ce2848c68e52649f5a0
Madi-S <khovansky99@gmail.com>
2021-01-25 18:35:16 +0600

    creating post page created

--- app/posts/blueprint.py
+++ app/posts/blueprint.py
@@ -2,11 +2,23 @@ from flask import Blueprint
 from flask import request
 from flask import render_template
 from models import Post, Tag
+from .forms import PostForm
+
 
 
 posts = Blueprint('posts', __name__, template_folder='templates')
 # the name 'posts' will be used in url_for function 
 # For instance, posts.index or posts.post_detail
+
+
+
+# Must be above than `index` because it will cosnider `create` as `q` parameter
+# http://localhost:5000/blog/create
+@posts.route('/create')
+def create_post():
+    form  = PostForm()
+    return render_template('posts/create_post.html', form=form)
+
 
 
 @posts.route('/')
@@ -20,7 +32,7 @@ def index():
     return render_template('posts/index.html', posts=posts)
 
 
-# https://localhost:5000/blog/first-post
+# http://localhost:5000/blog/first-post
 @posts.route('/<slug>')
 def post_detail(slug):
     # post not POST request but blog text
@@ -29,9 +41,11 @@ def post_detail(slug):
     return render_template('posts/post_detail.html', post=post, tags=tags)
 
 
-# slug for specific 'tags' -> e.g., https://localhost:5000/blog/tag/sports
+# slug for specific 'tags' -> e.g., http://localhost:5000/blog/tag/sports
 @posts.route('/tag/<slug>')
 def tag_detail(slug):
     tag = Tag.query.filter(Tag.slug==slug).first()
     posts = tag.posts #.all()
     return render_template('posts/tag_detail.html',tag=tag, posts=posts)
+
+
